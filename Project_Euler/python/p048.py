19e0f51857914d044d87b597827d44b38a65a39f
ajatkins <ajatkins@wsu.edu>
2021-01-02 15:32:17 -0800

    pe p048

---
+++ Project_Euler/python/p048.py
@@ -0,0 +1,15 @@
+﻿import os
+import time
+
+"""
+The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
+
+Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
+"""
+
+if __name__ == '__main__':
+    stime = time.time()
+    sum = 0
+    for i in range(1,1001):
+        sum += i**i
+    print("Solution: {0}\nRuntime: {1}".format(str(sum)[-10:],time.time()-stime))
