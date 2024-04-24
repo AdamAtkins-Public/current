71357879a7792591c028e8dafaf686bd242a90e0
ajatkins <ajatkins@wsu.edu>
2024-04-24 15:04:39 -0700

    add runtime calculator decorator

--- Project_Euler/python/pe_common.py
+++ Project_Euler/python/pe_common.py
@@ -1,9 +1,25 @@
 import os
 import math
+import time
+import functools
 
 '''
+    function 'prime_factors' is incorrect
+
+    function 'runtime_calculator' prints the results of wrapped function with a calculated runtime
 '''
 
+def runtime_calculator(function):
+    @functools.wraps(function)
+    def wrapper(*args,**kwargs):
+        start_time = time.time()
+        result = function(*args,**kwargs)
+        end_time = time.time()
+        print(f"Result:{result}\nRuntime:{end_time - start_time}")
+        return result
+    return wrapper
+
+@runtime_calculator
 def prime_factors(n):
     factors = {1:1}
     if n % 2 == 0:
@@ -31,12 +47,18 @@ def prime_factors(n):
 
     return factors
 
-if __name__ == '__main__':
+def test_wrapper():
+    prime_factors(6)
+
+def test_prime_factors():
     print(prime_factors(25))
     print(prime_factors(20))
     print(prime_factors(154))
-    print(prime_factors(1989))
-    print(prime_factors(14175))
-    print(prime_factors(600851475143))
+    #print(prime_factors(1989))
+    #print(prime_factors(14175))
+    #print(prime_factors(600851475143))
     #print(prime_factors(60085147514323941950914037510801238471857084560924856093459123489948325897234980759817239841))
-    print(prime_factors(60085147514323941950914037510801238471857084560924856093))
+    #print(prime_factors(60085147514323941950914037510801238471857084560924856093))
+
+if __name__ == '__main__':
+    test_wrapper()
